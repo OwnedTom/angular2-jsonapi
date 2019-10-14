4f63f9abb4ca26f59f35ffd1f9e37fce31e1513a
Tom Steele <tom@pxlwidgets.com>
2019-10-14 12:33:42 +0200

    Added Relationship decorator to replace broken usage of key

---
+++ src/decorators/relationship.decorator.ts
@@ -0,0 +1,12 @@
+export function Relationship(options: { resource: string } = { resource: '' }): PropertyDecorator {
+  return function (target: any, propertyName: string) {
+    const annotations = Reflect.getMetadata('Relationship', target) || [];
+
+    annotations.push({
+      propertyName,
+      relationship: options.resource || propertyName
+    });
+
+    Reflect.defineMetadata('HasMany', annotations, target);
+  };
+}
